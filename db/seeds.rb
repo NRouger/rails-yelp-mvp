a8f668420aeb9c91c61c1647cfc8042b93bb1666
nicolas rouger <nicorouger@outlook.com>
2022-11-16 16:49:31 +0100

    seeds completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+# pizza_napolitana = { name: 'Napoles', address: 'la esquina, Rt 01', phone_number: '0011111' }
+# pasta_mia = { name: 'Mamamia', address: 'MidStreet, Rt 1001', phone_number: '6235222' }
+
+# [pizza_napolitana, pasta_mia].each do |attributes|
+#   restaurant = Restaurant.create!(attributes)
+#   puts "Created #{restaurant.name}"
+# end
+
+5.times do
+  restaurant = Restaurant.create({
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Faker::Number.hexadecimal(digits: 3)
+    })
+    review = Review.new({
+      rating: rand(0..5),
+      content: Faker::TvShows::BojackHorseman.tongue_twister
+      })
+      review.restaurant = restaurant
+      review.save
+      puts "Restaurant with id: #{restaurant.id} has been created. And the review is #{review.content}"
+end
+
+puts 'Finished!'
